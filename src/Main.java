b822902ffc7862cb4c272d0df56529b540d4d2a6
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2023-12-02 19:31:04 +0300

    fix : database connection configuration

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        String jdbcUrl = "jdbc:jdbc://localhost:5432/wallet_management";
-        String user = "rotsy";
-        String password = "rotsy123";
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/wallet_management";
+        String user = "mandrindra";
+        String password = "Mandrindra$";
 
         try (Connection connection = DriverManager.getConnection(jdbcUrl, user, password)) {
             System.out.println("successful connection !");
