056075d6c3bbc75b4b588f20cc64151b6e22b6c1
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2023-12-03 17:37:42 +0300

    feat : add TransactionsCrudOperations class

---
+++ src/com/wallet/TransactionCrudOperations.java
@@ -0,0 +1,25 @@
+package com.wallet;
+
+import java.util.List;
+
+public class TransactionCrudOperations implements CrudOperations<Transaction>{
+    @Override
+    public List<Transaction> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> saveAll(List<Transaction> toSave) {
+        return null;
+    }
+
+    @Override
+    public Transaction save(Transaction toSave) {
+        return null;
+    }
+
+    @Override
+    public Transaction delete(Transaction toDelete) {
+        return null;
+    }
+}
