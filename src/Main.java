ab66a49b22a0027dc82950925da9b26d9a87693a
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2023-12-03 13:00:00 +0300

    feat : add currency, transaction and account table to the database

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,5 @@
-import java.io.InputStream;
-import java.util.Properties;
-
 public class Main {
     public static void main(String[] args) {
-        Properties prop = new Properties();
 
-        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
-            if (input == null) {
-                System.out.println("Sorry, unable to find config.properties");
-                return;
-            }
-            prop.load(input);
-
-            String jdbcUrl = prop.getProperty("jdbcUrl");
-            String user = prop.getProperty("dbUser");
-            String password = prop.getProperty("dbPassword");
-            System.out.println("Connection successfully");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- wallet_project.sql
+++ wallet_project.sql
@@ -7,3 +7,26 @@ BEGIN
     END IF;
 END $$;
 
+\c wallet_management;
+
+CREATE TABLE IF NOT EXISTS account(
+    account_id INT PRIMARY KEY,
+    customer_name varchar(200) NOT NULL,
+    balance DECIMAL(10, 2) DEFAULT 0.00,
+    password varchar(20) NOT NULL,
+    currency_id INT REFERENCES currency(currency_id)
+);
+
+CREATE TABLE IF NOT EXISTS "transaction"(
+    transaction_id INT PRIMARY KEY,
+    account_id INT REFERENCES account(account_id),
+    amount DECIMAL(10, 2) DEFAULT 0.00,
+    transaction_date DATE,
+    description varchar(200)
+);
+
+CREATE TABLE IF NOT EXISTS currency(
+    currency_id INT PRIMARY KEY,
+    currency_name varchar(200) NOT NULL,
+    currency_code varchar(3) NOT NULL
+);
