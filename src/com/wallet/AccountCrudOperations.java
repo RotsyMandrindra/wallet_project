58d7feb567ffb0cca0a155a546f2b2e23f1ff4e4
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2023-12-03 17:35:34 +0300

    feat : add AccountCrudOperations class

---
+++ src/com/wallet/AccountCrudOperations.java
@@ -0,0 +1,25 @@
+package com.wallet;
+
+import java.util.List;
+
+public class AccountCrudOperations implements CrudOperations<Account>{
+    @Override
+    public List<Account> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Account> saveAll(List<Account> toSave) {
+        return null;
+    }
+
+    @Override
+    public Account save(Account toSave) {
+        return null;
+    }
+
+    @Override
+    public Account delete(Account toDelete) {
+        return null;
+    }
+}
