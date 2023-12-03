0352a0b05cf6eeac28c6b1a9732c28a929391ecd
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2023-12-03 17:45:59 +0300

    feat : CurrencyCrudOperations class

---
+++ src/com/wallet/CurrencyCrudOperations.java
@@ -0,0 +1,25 @@
+package com.wallet;
+
+import java.util.List;
+
+public class CurrencyCrudOperations implements CrudOperations<Currency>{
+    @Override
+    public List<Currency> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Currency> saveAll(List<Currency> toSave) {
+        return null;
+    }
+
+    @Override
+    public Currency save(Currency toSave) {
+        return null;
+    }
+
+    @Override
+    public Currency delete(Currency toDelete) {
+        return null;
+    }
+}
