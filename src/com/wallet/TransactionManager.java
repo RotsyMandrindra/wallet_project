bfbe2d8ecbf57796f0a1a0e61efddd4f7cd67874
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2023-12-07 21:56:46 +0300

    fix : Transaction Manager class

--- src/com/wallet/TransactionManager.java
+++ src/com/wallet/TransactionManager.java
@@ -1,12 +1,9 @@
 package com.wallet;
 
-import javax.print.DocFlavor;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.sql.*;
 import java.util.Properties;
-
 
 public class TransactionManager {
     public static void main(String[] args) {
@@ -18,7 +15,7 @@ public class TransactionManager {
             String dbPassword = properties.getProperty("dbPassword");
 
             try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword)) {
-
+                performTransaction(connection, 1, 100.0, "credit", "Shoes", "Espece");
             }
         } catch (SQLException | IOException e) {
             e.printStackTrace();
@@ -29,30 +26,22 @@ public class TransactionManager {
         Properties properties = new Properties();
         try (InputStream input = TransactionManager.class.getClassLoader().getResourceAsStream("config.properties")) {
             if (input == null) {
-                System.out.println("Sorry, unable to find config.properties");
+                System.out.println("Sorry, I can't find config.properties");
                 return properties;
             }
             properties.load(input);
         }
         return properties;
     }
-    public static void performTransaction(int accountId, double amount, String transactionType, String description, String label) throws SQLException {
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            String accountType = getAccountType(connection, accountId);
 
-            if (("Banque".equals(accountType) || "Mobile Money".equals(accountType)) || "credit".equals(transactionType)) {
-                updateAccountBalance(connection, accountId, amount);
-            } else if ("Espèce".equals(accountType) && "debit".equals(transactionType)) {
-                if (isBalanceSufficient(connection, accountId, amount)) {
-                    updateAccountBalance(connection, accountId, -amount);
-                } else {
-                    throw new SQLException("Solde insuffisant pour effectuer la transaction.");
-                }
-            }
-            insertTransaction(connection, accountId, amount, transactionType, description, label);
+    public static void performTransaction(Connection connection, int accountId, double amount, String transactionType, String description, String label) throws SQLException {
+        String accountType = getAccountType(connection, accountId);
 
-            displayAccountDetails(connection, accountId);
-        }
+        updateAccountBalance(connection, accountId, amount, accountType, transactionType);
+
+        insertTransaction(connection, accountId, amount, transactionType, description, label);
+
+        displayAccountDetails(connection, accountId);
     }
 
     private static String getAccountType(Connection connection, int accountId) throws SQLException {
@@ -62,37 +51,32 @@ public class TransactionManager {
                 if (resultSet.next()) {
                     return resultSet.getString("account_type");
                 } else {
-                    throw new SQLException("Account ID" + accountId + " doesn't find");
+                    throw new SQLException("ID Account " + accountId + " doesn't found");
                 }
             }
         }
     }
 
-    private static void updateAccountBalance(Connection connection, int accountId, double amount) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("UPDATE account SET balance = balance + ? WHERE account_id = ?")) {
+    private static void updateAccountBalance(Connection connection, int accountId, double amount, String accountType, String transactionType) throws SQLException {
+        String updateSql;
+        if (("Bank".equals(accountType) || "Mobile Money".equals(accountType)) || "credit".equals(transactionType)) {
+            updateSql = "UPDATE account SET balance = balance + ? WHERE account_id = ?";
+        } else if ("Espece".equals(accountType) && "debit".equals(transactionType)) {
+            updateSql = "UPDATE account SET balance = balance - ? WHERE account_id = ?";
+        } else {
+            throw new SQLException("Type of account or transaction not supported.");
+        }
+
+        try (PreparedStatement statement = connection.prepareStatement(updateSql)) {
             statement.setDouble(1, amount);
             statement.setInt(2, accountId);
             statement.executeUpdate();
         }
     }
 
-    private static boolean isBalanceSufficient(Connection connection, int accountId, double amount) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("SELECT balance FROM account WHERE account_id = ?")) {
-            statement.setInt(1, accountId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    double balance = resultSet.getDouble("balance");
-                    return balance >= amount;
-                } else {
-                    throw new SQLException("Account ID " + accountId + " does not find.");
-                }
-            }
-        }
-    }
-
     private static void insertTransaction(Connection connection, int accountId, double amount, String transactionType, String description, String label) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO \"transaction\" (account_id, amount, transaction_date, description, transaction_type, label) " +
+                "INSERT INTO transaction (account_id, amount, transaction_date, description, transaction_type, label) " +
                         "VALUES (?, ?, CURRENT_TIMESTAMP, ?, ?, ?)")) {
             statement.setInt(1, accountId);
             statement.setDouble(2, amount);
@@ -117,11 +101,11 @@ public class TransactionManager {
                     String currencyName = resultSet.getString("currency_name");
 
                     System.out.println("Account ID: " + id);
-                    System.out.println("Account Name: " + name);
+                    System.out.println("Account name: " + name);
                     System.out.println("Balance: " + balance);
                     System.out.println("Currency: " + currencyName);
                 } else {
-                    throw new SQLException("Account ID " + accountId + " does not find.");
+                    throw new SQLException("Account ID " + accountId + " does not found.");
                 }
             }
         }
